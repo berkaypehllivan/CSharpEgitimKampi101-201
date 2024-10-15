bc79e4d0c399627495b1f78ec776edc53ed8add7
Berkay Pehlivan <berkaypehlivan75@gmail.com>
2024-10-15 10:59:35 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region Yazdırma Komutları Bölümü
+
+            //Yazdırma Komutları
+
+            //Console.WriteLine("Merhaba Dünya!");
+            //Console.Write("Merhabalar");
+
+            //Console.WriteLine("-----* Yiyecekler *-----");
+            //Console.WriteLine();
+            //Console.WriteLine("1-) Çorbalar");
+            //Console.WriteLine("2-) Ana Yemekler");
+            //Console.WriteLine("3-) Salatalar");
+            //Console.WriteLine("4-) Tatlılar");
+            //Console.WriteLine("5-) İçecekler");
+            //Console.WriteLine();
+            //Console.WriteLine("-----* Yiyecekler *-----");
+
+            #endregion
+
+            #region String Değişkenler
+
+            //string name;
+            //name = "Berkay";
+            //Console.WriteLine(name);
+
+            // Değişken isimlerinin baş harfinin küçük yazılmasına Camel Case denir. Bu yöntem okunabilirliği ve büyük projelerde
+            // isimlerin çakışması durumunu azaltmaya yarayabilir.
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Berkay";
+            //customerSurname = "Pehlivan";
+            //customerPhone = "+90 0507 107 36 62";
+            //customerEmail = "berkaypehlivan75@gmail.com";
+            //district = "Saruhanlı";
+            //city = "Manisa";
+
+            //Console.WriteLine("-----* Rezervasyon Kartı *-----");
+            //Console.WriteLine();
+            //Console.WriteLine("--------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email Adresi: " + customerEmail);
+            //Console.WriteLine("Adresi: " + district + " " + city);
+            //Console.WriteLine("--------------------------------");
+
+
+            #endregion
+
+            #region Int Değişkenler
+
+            //int number = 45;
+            //Console.WriteLine(number);
+
+            //int dönerPrice = 200;
+            //int pizzaPrice = 200;
+            //int friesPrice = 80;
+            //int ayranPrice = 25;
+            //int waterPrice = 15;
+            //int teaPrice = 15;
+
+            //Console.WriteLine("-----* Restoran Menü Fiyatı *-----");
+            //Console.WriteLine();
+            //Console.WriteLine("1-) Döner Fiyatı: " + dönerPrice + " TL");
+            //Console.WriteLine("2-) Pizza Fiyatı: " + pizzaPrice + " TL");
+            //Console.WriteLine("3-) Patates Kızartması Fiyatı: " + friesPrice + " TL");
+            //Console.WriteLine("4-) Ayran Fiyatı: " + ayranPrice + " TL");
+            //Console.WriteLine("5-) Su Fiyatı: " + waterPrice + " TL");
+            //Console.WriteLine("6-) Çay Fiyatı: " + teaPrice + " TL");
+            //Console.WriteLine();
+            //Console.WriteLine("*----- Restoran Menü Fiyatı *-----");
+
+            //Console.WriteLine();
+            //int dönerCount = 3;
+            //int pizzaCount = 2;
+            //int friesCount = 1;
+            //int ayranCount = 5;
+            //int waterCount = 4;
+            //int teaCount = 8;
+
+            //int totalDönerPrice;
+            //int totalPizzaPrice;
+            //int totalFriesPrice;
+            //int totalAyranPrice;
+            //int totalWaterPrice;
+            //int totalTeaPrice;
+
+            //totalDönerPrice = dönerCount * dönerPrice;
+            //totalPizzaPrice = pizzaCount * pizzaPrice;
+            //totalFriesPrice = friesCount * friesPrice;
+            //totalAyranPrice = ayranCount * ayranPrice;
+            //totalWaterPrice = waterCount * waterPrice;
+            //totalTeaPrice = teaCount * teaPrice;
+
+            //Console.WriteLine("--------------------------------");
+            //Console.WriteLine("Döner Tutarı: " + totalDönerPrice + " TL");
+            //Console.WriteLine("Pizza Tutarı: " + totalPizzaPrice + " TL");
+            //Console.WriteLine("Patates Kızartması Tutarı: " + totalFriesPrice + " TL");
+            //Console.WriteLine("Ayran Tutarı: " + totalAyranPrice + " TL");
+            //Console.WriteLine("Su Tutarı: " + totalWaterPrice + " TL");
+            //Console.WriteLine("Çay Tutarı: " + totalTeaPrice + " TL");
+            //Console.WriteLine();
+
+            //int totalPrice = totalDönerPrice + totalPizzaPrice + totalFriesPrice + totalAyranPrice + totalWaterPrice + totalTeaPrice;
+
+            //Console.WriteLine("Toplam Ödenecek Tutar: " + totalPrice + " TL");
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
